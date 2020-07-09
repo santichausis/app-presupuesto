b97e824f22fa211da7ae299c756a6e0d0a69d32a
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2020-07-09 10:09:14 -0300

    First commit

---
+++ src/components/Pregunta.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useState } from 'react'
+import Error from './Error'
+
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta }) => {
+
+    // Definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    // Función que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10))
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+        if( cantidad < 1 || isNaN(cantidad) ){
+            guardarError(true);
+            return;
+        }
+
+        // Si se pasa la validación
+        guardarError (false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+
+        // Actualizar pregunta
+        actualizarPregunta(false);
+    }
+
+    return (
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+
+            { error ? <Error mensaje="El presupuesto no es correcto"/> : null }
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+                <input 
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir Presupuesto"
+                />
+
+            </form>
+        </Fragment>
+    );
+}
+
+export default Pregunta;
